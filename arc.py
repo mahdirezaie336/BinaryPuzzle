56f84692e6bab70319d84eb97e257203b8747591
mahdi <mahdi.rezaie.336@gmail.com>
2021-06-19 00:22:40 +0430

    Define arc methods

--- arc.py
+++ arc.py
@@ -8,5 +8,19 @@ class Arc:
     def make_me_consistent(self, node):
         """ Makes head or tail arc consistent with respect to the other. """
         if node == self.__head:
-            node.make_arc_consistent_to()
+            node.make_arc_consistent_to(self.__tail)
+        elif node == self.__tail:
+            node.make_arc_consistent_to(self.__head)
         pass
+
+    def get_other_side(self, node):
+        """ Returns the others side node. """
+        if node == self.__head:
+            return self.__tail
+        if node == self.__tail:
+            return self.__head
+        return None
+
+    def get_constraint(self):
+        """ Returns the constraint function. """
+        return self.__cons
