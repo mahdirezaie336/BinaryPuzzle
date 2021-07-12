1502828fb71fbeb13b87a827bffa83cd6f3a41de
mahdi <mahdi.rezaie.336@gmail.com>
2021-07-13 02:46:25 +0430

    Define screen and cell sizes

--- constants.py
+++ constants.py
@@ -1,3 +1,8 @@
 class Consts:
 
     MAP_FILE = './maps/puzzle0.txt'
+
+    SCREEN_WIDTH = 800
+    SCREEN_HEIGHT = 600
+    BACKGROUND_COLOR = 198, 163, 138
+    SCREEN_MARGIN_SIZE = 40
--- screenmanager.py
+++ screenmanager.py
@@ -1,5 +1,37 @@
+import pygame
+
+from constants import Consts
+
+
 class Display:
 
     def __init__(self):
-        pass
+        self.__map = map_ + 'F'
+        self.__w = len(map_) + 1
+        self.__h = 4
+        w, h = self.__w, self.__h
 
+        # PyGame part
+        pygame.init()
+        sw, sh = Consts.SCREEN_WIDTH, Consts.SCREEN_HEIGHT
+        self.screen = pygame.display.set_mode((sw, sh))
+        self.screen.fill(Consts.BACKGROUND_COLOR)
+
+        # Setting cell size and other sizes
+        if w / h > sw / sh:
+            rect_width = sw - 2 * Consts.SCREEN_MARGIN_SIZE
+            cell_size = int(rect_width / w)
+            rect_height = cell_size * h
+        else:
+            rect_height = sh - 2 * Consts.SCREEN_MARGIN_SIZE
+            cell_size = int(rect_height / h)
+            rect_width = cell_size * w
+        self.cell_size = cell_size
+        self.rect_width = rect_width
+        self.rect_height = rect_height
+
+        self.draw_cells()
+        pygame.display.update()
+
+    def draw_cells(self):
+        pass
